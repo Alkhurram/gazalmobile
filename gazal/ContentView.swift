83499e7064d53c3a09848396314e056a026151fb
khurram <alkhurram@hotmail.com>
2021-01-03 11:49:45 +0400

    Initial Commit

---
+++ gazal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gazal
+//
+//  Created by MACBOOK PRO on 1/3/21.
+//  Copyright © 2021 MACBOOK PRO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
